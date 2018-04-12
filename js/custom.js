4f58a2f8c05dec202a42ef4f72546039f466f688
Minnesinger <js790427@gmail.com>
2018-04-12 11:49:06 +0800

    content update

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
   /* 啟用 scrollspy 選單 */
   $('body').scrollspy({
-    target: '#navbar',
+    target: '#navbarResponsive',
     offset: 50
   });
 
   /* 平順捲動的動作 */
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=\\#]:not([href=\\#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html,body').animate({
-            scrollTop: target.offset().top - 50
+            scrollTop: target.offset().top - 0
           }, 1000);
           return false;
         }
